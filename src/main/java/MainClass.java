c2b67693ad04c2f3b9850ffe4390a473032b4529
Ahmad Galion <galiun82@gmail.com>
2022-02-26 00:54:00 +0200

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,41 @@
+
+interface ExpensiveObject {
+    void process();
+}
+
+class ExpensiveObjectImpl implements ExpensiveObject {
+
+    public ExpensiveObjectImpl() {
+        heavyInitialConfiguration();
+    }
+
+    @Override
+    public void process() {
+        System.out.println("processing complete.");
+    }
+
+    private void heavyInitialConfiguration() {
+        System.out.println("Loading initial configuration...");
+    }
+
+}
+
+class ExpensiveObjectProxy implements ExpensiveObject {
+    private static ExpensiveObject object;
+
+    @Override
+    public void process() {
+        if (object == null) {
+            object = new ExpensiveObjectImpl();
+        }
+        object.process();
+    }
+}
+
+public class MainClass {
+    public static void main(String[] args) {
+        ExpensiveObject object = new ExpensiveObjectProxy();
+        object.process();
+        object.process();
+    }
+}
